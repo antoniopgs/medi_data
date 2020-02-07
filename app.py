7ef2de9e77ee4e78c96618a46befe876aa9f7f49
António Gonçalves <44982443+antoniopgs@users.noreply.github.com>
2020-02-07 18:11:12 +0000

    First Real Commit

--- app.py
+++ app.py
@@ -6,18 +6,26 @@ n = 0
 
 # ---------- BLOCK CLASS ----------
 class Block:
-    def __init__(self, data, previous_block=None):
+    def __init__(self, data, previous_block = None):
         self.data = data
-        self.time = datetime.now()
+        self.time = datetime.now().strftime("%A, %d %B %Y - %H:%M:%S")
+        self.previous_block = previous_block
 
     def __repr__(self):
         return f"""----- NEW RECORD -----
 Time: {self.time}
-Data: {self.data}"""
+Data: {self.data}
+"""
 
 
 # ---------- BLOCKCHAIN CLASS ----------
 class Blockchain:
+
+    # Protect Blockchain Memory Address from Prints:
+    def __repr__(self):
+        return None
+
+    # Create New Blockchain:
     def __init__(self):
         # Set "Tail Block" default as none. This will help identify if Blockchain is empty or not:
         self.tail_block = None
@@ -26,56 +34,55 @@ class Blockchain:
             address = hash(random.randint(1, 100))
             if address not in blockchains:
                 self.address = address
+                self.private_key = hash(random.randint(1,100))
+                print(f"""----- PERSONAL DATA -----
+Your Address: {self.address}
+Your Private Key: {self.private_key}
+WARNING: This data will never be shown again! Store it safely.
+""")
                 blockchains.append(self)
                 break
-                        
+
+    # Add new Block:
+    def add_block(self, address, private_key):
+        if address != self.address or private_key != self.private_key:
+            print("Invalid Credentials.")
+        else:
+            data = input("Insert Medical Data: ")
+            if not self.tail_block:
+                new_block = Block(data)
+            else:
+                new_block = Block(data, self.tail_block)
+            self.tail_block = new_block
+
     def __iter__(self):
         return self
 
     def __next__(self):
-        # If Blockchain is empty:
-        if not self.tail_block:
-            raise StopIteration
-        # If Blockchain is not empty:
+        global n
+        n += 1
+        # If it's the first iteration:
+        if n == 1:
+            return self.tail_block
+        # If it's after the first iteration:
         else:
-            # 'n' counts iterations of "__next__()", or how many times the "__next__()" function has been run.
-            global n
-            n += 1
-            # If it's the first "__next__()" iteration:
-            if n == 1:
-                return self.tail_block
-            # If it's after the first "__next__()" iteration:
+            current_block = self.tail_block
+            for i in range(n-1):
+                current_block = current_block.previous_block
+            if not current_block:
+                # Reset "n" to 0, so that next iteration works the same:
+                n = 0
+                raise StopIteration
             else:
-                current_block = self.tail_block
-                for i in range(n-1):
-                    current_block = current_block.previous_block
-                if not current_block:
-                    # Reset "n" to 0, so that next iteration works the same:
-                    n = 0
-                    raise StopIteration
-                else:
-                    return current_block
-            
-    def __repr__(self):
-        return f"{self.address}"
+                return current_block
 
-    def add_block(self, data):         
-    # Check to see if Blockchain is empty:
-        if not self.tail_block:
-            new_block = Block(data)
-        # If it's not empty, the new block's "previous block" will be the existing "tail block"
+
+    def view(self, address, private_key):
+        if address != self.address or private_key != self.private_key:
+            print("Invalid Credentials.")
         else:
-            new_block = Block(data, self.tail_block)
-        # Update "Tail Block" to the newly created block
-        self.tail_block = new_block
-
-    def view(self):
-        if not [block for block in self]:
-            print("This Blockchain is empty.\n")
-        else:
-            for block in self:
-                print(block)
-
-b1 = Blockchain()
-b1.add_block("Patient shows sign of x. Will start treatment.")
-b1.view()
+            if not self.tail_block:
+                print("Chain is empty.\n")
+            else:
+                for block in self:
+                    print(block)
